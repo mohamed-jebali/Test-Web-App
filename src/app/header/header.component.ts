e1039b625eabdf39ab4f17b5ba22864f0b481513
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-10-02 22:28:51 +0200

    imported bootstrap

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss', './path/to/bootstrap.css']
 })
 export class HeaderComponent {
 

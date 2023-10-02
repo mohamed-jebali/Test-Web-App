fded28c15a49d417f5f6995f847f332b3148767c
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-10-02 21:53:09 +0200

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule,

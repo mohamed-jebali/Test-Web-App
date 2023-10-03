0f93f4b3372efb02e12339e3cc5148040115d056
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-10-03 21:17:37 +0200

    added db files to main component

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
-import data from '../../assets/db.json';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../config/config.service';
 
 @Component({
   selector: 'main-content',
   templateUrl: './main-content.component.html',
   styleUrls: ['./main-content.component.scss']
 })
-export class MainContentComponent {
+export class MainContentComponent implements OnInit {
+  dati: any;
 
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+    this.dataService.getData().subscribe(data => {
+      this.dati = data;
+      console.log(this.dati)
+    });
+  }
 }

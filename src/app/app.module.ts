7b375baa2dbf1c0f85b9163be6b276609f61c2fc
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-10-03 18:32:32 +0200

    added db.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { MainContentComponent } from './main-content/main-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     MainContentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import data from '../../assets/db.json';
 
 @Component({
   selector: 'main-content',
